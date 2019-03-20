00dd579404c66bed736a8114a088ed002520b341
Charlie Root <root@wanA.localdomain>
2019-03-20 08:18:38 +0800

    update server.c

--- server.c
+++ server.c
@@ -12,7 +12,10 @@ int main(int argc, char const *argv[])
 
 { 	
 	setbuf(stdout,NULL);
-	printf("Creating socket..");
+	
+printf("*****************************************************\n\n");
+
+
 	int server_fd, new_socket, valread; 
 
 	struct sockaddr_in address; 
@@ -23,19 +26,15 @@ int main(int argc, char const *argv[])
 
 	char buffer[1024] = {0}; 
 
-	char *hello = "     Bonjour!"; 
+	char *hello = "Bonjour! (from server)"; 
 
 	
 	// Creating socket file descriptor 
 
 	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
-
 	{ 
-
 		perror("socket failed"); 
-
 		exit(EXIT_FAILURE); 
-
 	} 
 
 	
@@ -49,46 +48,43 @@ int main(int argc, char const *argv[])
 
 	address.sin_port = htons( PORT ); 
 
+
+	printf("\t***Socket is open");
+
+
+
+	// Forcefully attaching socket to the port 8080 
+
+	if (bind(server_fd, (struct sockaddr *)&address,sizeof(address))<0) 
+	{ 
+
+	   perror("bind failed"); 
+	   exit(EXIT_FAILURE); 
+	} 
+	    
+	if (listen(server_fd, 3) < 0) 
+	{ 
+	     perror("listen"); 
+	     exit(EXIT_FAILURE); 
+	} 
+
+   	if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) 
+	{ 
+	     perror("accept"); 
+	     exit(EXIT_FAILURE); 
+	} 
+
+	valread = read( new_socket , buffer, 1024); 
+	
+	printf("%s\n",buffer); 
 	
 
-    // Forcefully attaching socket to the port 8080 
+printf("\n\t***Socket closed\n\n");
+printf("*****************************************************\n\n\n");	
 
-    if (bind(server_fd, (struct sockaddr *)&address,sizeof(address))<0) 
-    { 
+	send(new_socket , hello , strlen(hello) , 0 ); 
 
-        perror("bind failed"); 
+	return 0; 
 
-        exit(EXIT_FAILURE); 
-
-    } 
-    
-    
-    if (listen(server_fd, 3) < 0) 
-
-    { 
-
-        perror("listen"); 
-
-        exit(EXIT_FAILURE); 
-
-    } 
-
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) 
-
-    { 
-
-        perror("accept"); 
-
-        exit(EXIT_FAILURE); 
-
-    } 
-
-    valread = read( new_socket , buffer, 1024); 
-
-    printf("%s\n",buffer ); 
-
-    send(new_socket , hello , strlen(hello) , 0 ); 
-
-    return 0; 
 
 } 
