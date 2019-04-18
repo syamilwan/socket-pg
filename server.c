a2448a533ff19fb8768b1cc3b58e9428146e63bd
Wan Muhammad Syamil <48310398+syamilwan@users.noreply.github.com>
2019-04-19 02:48:31 +0800

    Update server.c

--- server.c
+++ server.c
@@ -1,5 +1,3 @@
-// Server side C/C++ program to demonstrate Socket programming 12312312321313123
-
 #include <unistd.h> 
 #include <stdio.h> 
 #include <sys/socket.h> 
@@ -12,64 +10,40 @@ int main(int argc, char const *argv[])
 
 { 	
 	setbuf(stdout,NULL);
-	
-printf("*****************************************************\n\n");
-
+	printf("*****************************************************\n\n");
 
 	int server_fd, new_socket, valread; 
-
 	struct sockaddr_in address; 
-
 	int opt = 1; 
-
 	int addrlen = sizeof(address); 
-
 	char buffer[1024] = {0}; 
-
 	char *hello = "Bonjour! (from server)"; 
 
-	
 	// Creating socket file descriptor 
-
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
-	{ 
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) { 
 		perror("socket failed"); 
 		exit(EXIT_FAILURE); 
 	} 
 
-	
-
 	// Forcefully attaching socket to the port 8080 
-
-
 	address.sin_family = AF_INET; 
-
 	address.sin_addr.s_addr = INADDR_ANY; 
-
 	address.sin_port = htons( PORT ); 
-
 
 	printf("\t***Socket is open");
 
-
-
 	// Forcefully attaching socket to the port 8080 
-
-	if (bind(server_fd, (struct sockaddr *)&address,sizeof(address))<0) 
-	{ 
-
+	if (bind(server_fd, (struct sockaddr *)&address,sizeof(address))<0) { 
 	   perror("bind failed"); 
 	   exit(EXIT_FAILURE); 
 	} 
 	    
-	if (listen(server_fd, 3) < 0) 
-	{ 
+	if (listen(server_fd, 3) < 0) { 
 	     perror("listen"); 
 	     exit(EXIT_FAILURE); 
 	} 
 
-   	if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) 
-	{ 
+   	if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) { 
 	     perror("accept"); 
 	     exit(EXIT_FAILURE); 
 	} 
@@ -77,14 +51,10 @@ printf("*****************************************************\n\n");
 	valread = read( new_socket , buffer, 1024); 
 	
 	printf("%s\n",buffer); 
-	
-
-printf("\n\t***Socket closed\n\n");
-printf("*****************************************************\n\n\n");	
+	printf("\n\t***Socket closed\n\n");
+	printf("*****************************************************\n\n\n");	
 
 	send(new_socket , hello , strlen(hello) , 0 ); 
 
 	return 0; 
-
-
 } 
