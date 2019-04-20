5e476162f1520f61ea4f9aa2b2dd7cef25ec4ba8
Wan Muhammad Syamil <48310398+syamilwan@users.noreply.github.com>
2019-04-20 15:35:27 +0800

    Update smtpClient.java

--- smtpClient.java
+++ smtpClient.java
@@ -3,41 +3,41 @@ import java.net.*;
 import java.util.Scanner; 
 
 public class smtpClient {
-
     public static void main(String[] args) throws IOException {
     final int PORT_NUMBER = 999;
     final String HOSTNAME = "192.168.59.72";
+    char newEmail;	    
     boolean stop = false;
-    //Attempt to connect
-    try {
-        Socket sock = new Socket(HOSTNAME, PORT_NUMBER);
-        PrintWriter out = new PrintWriter(sock.getOutputStream(),true);
-	Scanner input = new Scanner(System.in);
-        //BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-	
-	while(stop!=true){
-		System.out.println("\n\tNew Email\n\n");
-		
-		//Output to server
-		System.out.println("\nRecipient: ");
-		out.print("\nRecipient: "+input.next());
-		System.out.println("\nTitle: ");
-		out.print("\nTitle: "+input.next());
-		System.out.println("\nMessages: ");
-		out.print("\nMessages: "+input.next());
-		out.flush();
-		}
-	    
-	//System.out.println("\t\t"+in.readLine());
-	System.out.println("\n\n\tSession End\n");
-	        
-        out.close();
-	//in.close();
-        sock.close();
-        
-        } 
-    catch(Exception e) {
-        e.printStackTrace();
-        }
+    	//Attempt to connect
+	try {
+		Socket sock = new Socket(HOSTNAME, PORT_NUMBER);
+		PrintWriter out = new PrintWriter(sock.getOutputStream(),true);
+		Scanner input = new Scanner(System.in);
+		//BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+		System.out.println("\n\n\Connected\n");
+		do{	System.out.println("\n\tSend New Email? (Y/N)\n\n");
+			newEmail=input.next();
+			if(netEmail == 'N' || netEmail == 'n'){
+				stop = true;}
+			//Output to server
+			System.out.print("\nRecipient: ");
+			out.println(input.next());
+			System.out.print("\nTitle: ");
+			out.println(input.next());
+			System.out.print("\nMessages: ");
+			out.println(input.next());
+			out.flush();
+		}while(stop!=true)
+
+		//System.out.println("\t\t"+in.readLine());
+		System.out.println("\n\n\Disconnected\n");
+
+		out.close();
+		//in.close();
+		sock.close();
+	    } 
+	catch(Exception e) {
+		e.printStackTrace();
+	}
     }
 }
