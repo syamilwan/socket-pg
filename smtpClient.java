bb79065df3034fd72410430542a89f2f60f73375
Wan Muhammad Syamil <48310398+syamilwan@users.noreply.github.com>
2019-04-20 10:55:37 +0800

    Update and rename smtpC.java to smtpClient.java

--- smtpClient.java
+++ smtpClient.java
@@ -2,12 +2,12 @@ import java.io.*;
 import java.net.*;
 import java.util.Scanner; 
 
-public class smtpC {
+public class smtpClient {
 
     public static void main(String[] args) throws IOException {
     final int PORT_NUMBER = 25;
     final String HOSTNAME = "192.168.59.71";
-    
+    bool stop = false;
     //Attempt to connect
     try {
         Socket sock = new Socket(HOSTNAME, PORT_NUMBER);
@@ -15,20 +15,22 @@ public class smtpC {
 	Scanner input = new Scanner(System.in);
         //BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
 	
-	System.out.println("\n\tSession Start\n\n");
-        
-	//Output to server
-        System.out.println("\nRecipient: ");
-	out.print(input.next());
-	System.out.println("\nTitle: ");
-	out.print(input.next());
-	System.out.println("\nMessages: ");
-	out.print(input.next());
-	out.flush();
-        
+	while(stop!=true){
+		System.out.println("\n\tNew Email\n\n");
+		
+		//Output to server
+		System.out.println("\nRecipient: ");
+		out.print("\nRecipient: "+input.next());
+		System.out.println("\nTitle: ");
+		out.print("\nTitle: "+input.next());
+		System.out.println("\nMessages: ");
+		out.print("\nMessages: "+input.next());
+		out.flush();
+		}
+	    
 	//System.out.println("\t\t"+in.readLine());
 	System.out.println("\n\n\tSession End\n");
-        
+	        
         out.close();
 	//in.close();
         sock.close();
